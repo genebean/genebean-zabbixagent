48105bda671f29e1ede99b118f5eb0a37d09aa4d
Gene Liverman <gliverma@westga.edu>
2015-01-25 14:26:33 -0500

    Fixed validation, added tests.

---
+++ spec/classes/zabbixagent_config_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe 'zabbixagent::config' do
+
+  describe 'with servers and servers_active params set' do
+    let :facts do
+      {
+        :kernel          => 'Linux',
+        :osfamily        => 'RedHat',
+        :operatingsystem => 'RedHat'
+      }
+    end
+
+    context 'to a single server' do
+      let :pre_condition do
+        "class {'zabbixagent':
+          servers        => 'zabbix.example.com',
+          servers_active => 'zabbix.example.com',
+        }"
+      end
+
+      it 'should set Servers and ServersActive' do
+        should contain_ini_setting('servers setting').with_value('zabbix.example.com')
+        should contain_ini_setting('servers active setting').with_value('zabbix.example.com')
+      end
+    end
+
+    context 'to an array of servers' do
+      let :pre_condition do
+        "class {'zabbixagent':
+          servers        => ['zabbix.example.com', 'node.example.com'],
+          servers_active => ['zabbix.example.com', 'node.example.com'],
+        }"
+      end
+
+      it 'should set Servers and ServersActive' do
+        should contain_ini_setting('servers setting').with_value('zabbix.example.com,node.example.com')
+        should contain_ini_setting('servers active setting').with_value('zabbix.example.com,node.example.com')
+      end
+    end
+
+  end
+end
--- spec/classes/zabbixagent_init_spec.rb
+++ spec/classes/zabbixagent_init_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe 'zabbixagent' do
 
-  # Running an RedHat OS.
   context 'On a RedHat OS with repo management enabled' do
     let :facts do
       {
@@ -25,6 +24,49 @@ describe 'zabbixagent' do
     it { should contain_class('zabbixagent::install')}
     it { should contain_class('zabbixagent::config')}
     it { should contain_class('zabbixagent::service')}
+
+  end
+
+  describe 'with server and server_active params set' do
+    let :facts do
+      {
+        :kernel          => 'Linux',
+        :osfamily        => 'RedHat',
+        :operatingsystem => 'RedHat'
+      }
+    end
+
+    context 'to a single server' do
+      let :pre_condition do
+        "class {'zabbixagent':
+          servers        => 'zabbix.example.com',
+          servers_active => 'zabbix.example.com',
+        }"
+      end
+
+      it 'should pass parameters to zabbixagent::config' do
+        should contain_class('zabbixagent::config').with(
+          'servers'        => 'zabbix.example.com',
+          'servers_active' => 'zabbix.example.com',
+        )
+      end
+    end
+
+    context 'to an array of servers' do
+      let :pre_condition do
+        "class {'zabbixagent':
+          servers        => ['zabbix.example.com', 'node.example.com'],
+          servers_active => ['zabbix.example.com', 'node.example.com'],
+        }"
+      end
+
+      it 'should pass parameters to zabbixagent::config' do
+        should contain_class('zabbixagent::config').with(
+          'servers'        => '["zabbix.example.com", "node.example.com"]',
+          'servers_active' => '["zabbix.example.com", "node.example.com"]',
+        )
+      end
+    end
 
   end
 end

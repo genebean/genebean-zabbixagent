90075eeb0acf86d95b69eb7fd0ee6617d9552e70
Gene Liverman <gliverma@westga.edu>
2015-02-11 22:26:50 -0500

    updated tests

--- spec/classes/zabbixagent_init_spec.rb
+++ spec/classes/zabbixagent_init_spec.rb
@@ -39,15 +39,15 @@ describe 'zabbixagent' do
     context 'to a single server' do
       let :pre_condition do
         "class {'zabbixagent':
-          servers        => 'zabbix.example.com',
-          servers_active => 'zabbix.example.com',
+          server        => 'zabbix.example.com',
+          server_active => 'zabbix.example.com',
         }"
       end
 
       it 'should pass parameters to zabbixagent::config' do
         should contain_class('zabbixagent::config').with(
-          'servers'        => 'zabbix.example.com',
-          'servers_active' => 'zabbix.example.com',
+          'server'        => 'zabbix.example.com',
+          'server_active' => 'zabbix.example.com',
         )
       end
     end
@@ -55,15 +55,15 @@ describe 'zabbixagent' do
     context 'to an array of servers' do
       let :pre_condition do
         "class {'zabbixagent':
-          servers        => ['zabbix.example.com', 'node.example.com'],
-          servers_active => ['zabbix.example.com', 'node.example.com'],
+          server        => ['zabbix.example.com', 'node.example.com'],
+          server_active => ['zabbix.example.com', 'node.example.com'],
         }"
       end
 
       it 'should pass parameters to zabbixagent::config' do
         should contain_class('zabbixagent::config').with(
-          'servers'        => '["zabbix.example.com", "node.example.com"]',
-          'servers_active' => '["zabbix.example.com", "node.example.com"]',
+          'server'        => '["zabbix.example.com", "node.example.com"]',
+          'server_active' => '["zabbix.example.com", "node.example.com"]',
         )
       end
     end

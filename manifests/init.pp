0ae7ef65d899727d1c310c8dcafdecae44f68e5f
Gene Liverman <gliverma@westga.edu>
2015-02-11 20:36:32 -0500

    Added new params, accounted for removed ones.

--- manifests/init.pp
+++ manifests/init.pp
@@ -46,16 +46,75 @@
 # Sample Usage:
 #
 class zabbixagent (
-  $ensure_setting     = $::zabbixagent::params::ensure_setting,
-  $hostname           = $::zabbixagent::params::hostname,
-  $include_dir        = $::zabbixagent::params::include_dir,
-  $include_file       = $::zabbixagent::params::include_file,
-  $logfile            = $::zabbixagent::params::logfile,
-  $manage_repo_epel   = $::zabbixagent::params::manage_repo_epel,
-  $manage_repo_zabbix = $::zabbixagent::params::manage_repo_zabbix,
-  $servers            = $::zabbixagent::params::servers,
-  $servers_active     = $::zabbixagent::params::servers_active,
+  # depreciated vars
+  $include_dir            = undef,
+  $include_file           = undef,
+  $logfile                = undef,
+  $servers                = undef,
+  $servers_active         = undef,
+
+  # preinstall settings
+  $manage_repo_epel       = $::zabbixagent::params::manage_repo_epel,
+  $manage_repo_zabbix     = $::zabbixagent::params::manage_repo_zabbix,
+
+  # install setting
+  $ensure_setting         = $::zabbixagent::params::ensure_setting,
+
+  # config file settings
+  $alias                  = $::zabbixagent::params::alias,
+  $allow_root             = $::zabbixagent::params::allow_root,
+  $buffer_send            = $::zabbixagent::params::buffer_send,
+  $buffer_size            = $::zabbixagent::params::buffer_size,
+  $debug_level            = $::zabbixagent::params::debug_level,
+  $enable_remote_commands = $::zabbixagent::params::enable_remote_commands,
+  $host_metadata_item     = $::zabbixagent::params::host_metadata_item,
+  $host_metadata          = $::zabbixagent::params::host_metadata,
+  $hostname_item          = $::zabbixagent::params::hostname_item,
+  $hostname               = $::zabbixagent::params::hostname,
+  $include                = $::zabbixagent::params::include,
+  $listen_ip              = $::zabbixagent::params::listen_ip,
+  $listen_port            = $::zabbixagent::params::listen_port,
+  $load_module_path       = $::zabbixagent::params::load_module_path,
+  $load_module            = $::zabbixagent::params::load_module,
+  $log_file_size          = $::zabbixagent::params::log_file_size,
+  $log_file               = $::zabbixagent::params::log_file,
+  $log_remote_commands    = $::zabbixagent::params::log_remote_commands,
+  $max_lines_per_second   = $::zabbixagent::params::max_lines_per_second,
+  $perf_counter           = $::zabbixagent::params::perf_counter,
+  $pid_file               = $::zabbixagent::params::pid_file,
+  $refresh_active_checks  = $::zabbixagent::params::refresh_active_checks,
+  $server_active          = $::zabbixagent::params::server_active,
+  $server                 = $::zabbixagent::params::server,
+  $source_ip              = $::zabbixagent::params::source_ip,
+  $start_agents           = $::zabbixagent::params::start_agents,
+  $timeout                = $::zabbixagent::params::timeout,
+  $unsafe_user_parameters = $::zabbixagent::params::unsafe_user_parameters,
+  $user_parameter         = $::zabbixagent::params::user_parameter,
+  $user                   = $::zabbixagent::params::user,
 ) inherits ::zabbixagent::params {
+  # these should not be used as they are pre v2.1
+  $depreciation_msg = 'was removed in v2.1. Please update your manifests and/or hiera data.'
+
+  if defined($include_dir) {
+    fail("\$include_dir #{depreciation_msg}")
+  }
+
+  if defined($include_file) {
+    fail("\$include_file #{depreciation_msg}")
+  }
+
+  if defined($logfile) {
+    fail("\$logfile #{depreciation_msg}")
+  }
+
+  if defined($servers) {
+    fail("\$servers #{depreciation_msg}")
+  }
+
+  if defined($servers_active) {
+    fail("\$servers_active #{depreciation_msg}")
+  }
+
   # validate booleans
   validate_bool($manage_repo_epel)
   validate_bool($manage_repo_zabbix)
@@ -63,15 +122,12 @@ class zabbixagent (
   # validate strings
   validate_string($ensure_setting)
   validate_string($hostname)
-  validate_string($include_dir)
-  validate_string($include_file)
-  validate_string($logfile)
 
-  if (! (is_string($servers) or is_array($servers))) {
+  if (!(is_string($server) or is_array($server))) {
     fail('$servers must be either a string or an array')
   }
 
-  if (! (is_string($servers_active) or is_array($servers_active))) {
+  if (!(is_string($server_active) or is_array($server_active))) {
     fail('$servers_active must be either a string or an array')
   }
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,47 @@
 # Parameters used by this module
 class zabbixagent::params {
-  $ensure_setting     = 'present'
-  $hostname           = downcase($::fqdn)
-  $include_dir        = 'zabbix_agentd.d'
-  $include_file       = ''
-  $manage_repo_epel   = false
-  $manage_repo_zabbix = false
-  $servers            = '127.0.0.1'
-  $servers_active     = '127.0.0.1'
+  # perinstall settings
+  $manage_repo_epel       = false
+  $manage_repo_zabbix     = false
+
+  # install settings
+  $ensure_setting         = 'present'
+
+  # config settings
+  $alias                  = undef
+  $allow_root             = undef
+  $buffer_send            = undef
+  $buffer_size            = undef
+  $debug_level            = undef
+  $enable_remote_commands = undef
+  $host_metadata_item     = undef
+  $host_metadata          = undef
+  $hostname_item          = undef
+  $hostname               = downcase($::fqdn)
+  $include                = undef
+  $listen_ip              = undef
+  $listen_port            = undef
+  $load_module_path       = undef
+  $load_module            = undef
+  $log_file_size          = undef
+  $log_file               = undef
+  $log_remote_commands    = undef
+  $max_lines_per_second   = undef
+  $perf_counter           = undef
+  $pid_file               = undef
+  $refresh_active_checks  = undef
+  $server_active          = '127.0.0.1'
+  $server                 = '127.0.0.1'
+  $source_ip              = undef
+  $start_agents           = undef
+  $timeout                = undef
+  $unsafe_user_parameters = undef
+  $user_parameter         = undef
+  $user                   = undef
 
   # this isn't a parameter but, since this class is inherited by all classes
   # it is a good place to put this message so that it's the same everywhere
-  $fail_message       = "${::kernel} is not yet supported by this module."
+  $fail_message           = "${::kernel} is not yet supported by this module."
 
   case $::kernel {
     'Linux'   : {

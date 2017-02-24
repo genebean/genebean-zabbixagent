8cc69e8a200f8f419bc8699f52fc7dadd77f665d
Jake Spain <jakespain1@gmail.com>
2017-02-24 17:00:35 -0500

    Added support for Zabbix 3.2

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,11 +2,20 @@
 class zabbixagent::install (
   $ensure_setting         = $::zabbixagent::ensure_setting,
   $custom_require_linux   = $::zabbixagent::custom_require_linux,
-  $custom_require_windows = $::zabbixagent::custom_require_windows) {
+  $custom_require_windows = $::zabbixagent::custom_require_windows,
+  $package_name           = $::zabbixagent::package_name,
+  $version                = $::zabbixagent::version) {
   case $::kernel {
     'Linux'   : {
 
-      package { 'zabbix-agent':
+      if ($package_name != 'zabbix-agent') {
+        package { 'zabbix-agent':
+          ensure => absent,
+          before => Package[$package_name],
+        }
+      }
+
+      package { $package_name:
         ensure  => $ensure_setting,
         notify  => Service[$::zabbixagent::params::service_name],
         require => $custom_require_linux,
@@ -15,7 +24,7 @@ class zabbixagent::install (
     } # end Linux
 
     'Windows' : {
-      package { 'zabbix-agent':
+      package { $package_name:
         ensure   => $ensure_setting,
         provider => 'chocolatey',
         notify   => Service['zabbix-agent'],
--- spec/classes/zabbixagent_preinstall_spec.rb
+++ spec/classes/zabbixagent_preinstall_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'zabbixagent::preinstall' do
 
-  # Running an RedHat OS.
-  context 'On a RedHat OS with repo management enabled' do
+  # Running an RedHat 7.
+  context 'On a RedHat 7 with repo management enabled' do
     let :facts do
       {
           :kernel                    => 'Linux',
@@ -17,11 +17,13 @@ describe 'zabbixagent::preinstall' do
       "class {'zabbixagent':
         manage_repo_zabbix => true,
         manage_repo_epel   => true,
+        version            => '3.2',
       }"
     end
 
     it 'should create zabbix.repo' do
-      should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/baseurl=http:\/\/repo.zabbix.com\/zabbix\/2.4\/rhel\/7\/\$basearch\//)
+      should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/baseurl=http:\/\/repo.zabbix.com\/zabbix\/3.2\/rhel\/7\/\$basearch\//)
+      should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/gpgkey=http:\/\/repo.zabbix.com\/RPM-GPG-KEY-ZABBIX-A14FE591/)
     end
 
     it 'should create epel.repo' do
@@ -30,8 +32,62 @@ describe 'zabbixagent::preinstall' do
 
   end
 
+  # Running an RedHat 5.
+  context 'On a RedHat 5 with repo management enabled' do
+    let :facts do
+      {
+          :kernel                    => 'Linux',
+          :osfamily                  => 'RedHat',
+          :operatingsystem           => 'RedHat',
+          :operatingsystemmajrelease => '5'
+      }
+    end
+
+    let :pre_condition do
+      "class {'zabbixagent':
+        manage_repo_zabbix => true,
+        manage_repo_epel   => true,
+        version            => '3.2',
+      }"
+    end
+
+    it 'should create zabbix.repo' do
+      should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/baseurl=http:\/\/repo.zabbix.com\/zabbix\/3.2\/rhel\/5\/\$basearch\//)
+      should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/gpgkey=http:\/\/repo.zabbix.com\/RPM-GPG-KEY-ZABBIX-A14FE591-EL5/)
+    end
+
+    it 'should create epel.repo' do
+      should contain_file('/etc/yum.repos.d/epel.repo').with_content(/mirrorlist=https:\/\/mirrors.fedoraproject.org\/.*epel-5/)
+    end
+
+  end
+
+  # Running an Ubuntu 14.04.
+  context 'On a Ubuntu 14 with repo management enabled' do
+    let :facts do
+      {
+          :kernel                    => 'Linux',
+          :osfamily                  => 'Debian',
+          :operatingsystem           => 'Ubuntu',
+          :lsbdistcodename           => 'trusty',
+          :operatingsystemmajrelease => '14.04'
+      }
+    end
+
+    let :pre_condition do
+      "class {'zabbixagent':
+        manage_repo_zabbix => true,
+        version            => '3.2',
+      }"
+    end
+
+    it 'should create zabbix.list' do
+      should contain_file('/etc/apt/sources.list.d/zabbix.list').with_content(/deb http:\/\/repo.zabbix.com\/zabbix\/3.2\/ubuntu trusty main/)
+    end
+  end
+
   # Running an OpenSuSE OS.
-  context 'On a OpenSuSE Leap 42.1 OS with repo management enabled' do
+  context 'On a OpenSuSE Leap 42.1 with repo management enabled' do
     let :facts do
       {
           :kernel                 => 'Linux',
@@ -48,12 +104,12 @@ describe 'zabbixagent::preinstall' do
     end
 
     it 'should create server_monitoring.repo' do
-      should contain_file('/etc/zypp/repos.d/server_monitoring.repo').with_content(/baseurl=http:\/\/download.opensuse.org\/repositories\/server:\/monitoring\/openSUSE_Leap_42.1\//)
+      should contain_file('/etc/zypp/repos.d/server_monitoring.repo').with_content(/baseurl=http:\/\/download.opensuse.org\/repositories\/home:\/ecsos:\/monitoring\/openSUSE_Leap_42.1\//)
     end
   end
 
   # Running an SLES OS.
-  context 'On a SLES 12.1 OS with repo management enabled' do
+  context 'On a SLES 12.1 with repo management enabled' do
     let :facts do
       {
           :kernel                 => 'Linux',
@@ -66,15 +122,16 @@ describe 'zabbixagent::preinstall' do
     let :pre_condition do
       "class {'zabbixagent':
         manage_repo_zabbix => true,
+        version            => '3.2',
       }"
     end
 
     it 'should create server_monitoring.repo' do
-      should contain_file('/etc/zypp/repos.d/server_monitoring.repo').with_content(/baseurl=http:\/\/download.opensuse.org\/repositories\/server:\/monitoring\/SLE_12_SP1\//)
+      should contain_file('/etc/zypp/repos.d/server_monitoring.repo').with_content(/baseurl=http:\/\/download.opensuse.org\/repositories\/home:\/ecsos:\/monitoring\/openSUSE_Leap_42.2\//)
     end
   end
 
-  context 'On a SLES 11.3 OS with repo management enabled' do
+  context 'On a SLES 11.3 with repo management enabled' do
     let :facts do
       {
           :kernel                 => 'Linux',

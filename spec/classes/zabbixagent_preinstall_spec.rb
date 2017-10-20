7ee61452da5f7748da42d6ccb5b2fec94f0277c7
Gene Liverman <gene@technicalissues.us>
2017-10-20 15:29:59 -0700

    Reworked to clean things up a bit

--- spec/classes/zabbixagent_preinstall_spec.rb
+++ spec/classes/zabbixagent_preinstall_spec.rb
@@ -2,47 +2,37 @@ require 'spec_helper'
 
 describe 'zabbixagent::preinstall' do
   on_supported_os.each do |os, facts|
-    context "on #{os}" do
+    context "on #{os} with repo management enabled" do
       let(:facts) do
         facts
       end
 
+      let :pre_condition do
+        "class {'zabbixagent':
+          manage_repo_zabbix => true,
+          manage_repo_epel   => true,
+          version            => '3.2',
+        }"
+      end
       case facts[:os]['family']
       when 'RedHat'
-        context 'with repo management enabled' do
-          let :pre_condition do
-            "class {'zabbixagent':
-              manage_repo_zabbix => true,
-              manage_repo_epel   => true,
-              version            => '3.2',
-            }"
-          end
-
-          it 'should create epel.repo' do
-            should contain_file('/etc/yum.repos.d/epel.repo').with_content(/mirrorlist=https:\/\/mirrors.fedoraproject.org\/.*epel-#{facts[:os]['release']['major']}/)
-          end
-
-          case facts[:os]['release']['major']
-          when '5'
-            it 'should create zabbix.repo' do
-              should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/baseurl=http:\/\/repo.zabbix.com\/zabbix\/3.2\/rhel\/#{facts[:os]['release']['major']}\/\$basearch\//)
-              should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/gpgkey=http:\/\/repo.zabbix.com\/RPM-GPG-KEY-ZABBIX-A14FE591-EL5/)
-            end
-          else
-            it 'should create zabbix.repo' do
-              should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/baseurl=http:\/\/repo.zabbix.com\/zabbix\/3.2\/rhel\/#{facts[:os]['release']['major']}\/\$basearch\//)
-              should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/gpgkey=http:\/\/repo.zabbix.com\/RPM-GPG-KEY-ZABBIX-A14FE591/)
-            end
-          end # ends case facts[:operatingsystemmajrelease]
-        end # ends context 'with repo management enabled' do
-      when 'Debian'
-        let :pre_condition do
-          "class {'zabbixagent':
-            manage_repo_zabbix => true,
-            version            => '3.2',
-          }"
+        it 'should create epel.repo' do
+          should contain_file('/etc/yum.repos.d/epel.repo').with_content(/mirrorlist=https:\/\/mirrors.fedoraproject.org\/.*epel-#{facts[:os]['release']['major']}/)
         end
 
+        case facts[:os]['release']['major']
+        when '5'
+          it 'should create zabbix.repo' do
+            should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/baseurl=http:\/\/repo.zabbix.com\/zabbix\/3.2\/rhel\/#{facts[:os]['release']['major']}\/\$basearch\//)
+            should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/gpgkey=http:\/\/repo.zabbix.com\/RPM-GPG-KEY-ZABBIX-A14FE591-EL5/)
+          end
+        else
+          it 'should create zabbix.repo' do
+            should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/baseurl=http:\/\/repo.zabbix.com\/zabbix\/3.2\/rhel\/#{facts[:os]['release']['major']}\/\$basearch\//)
+            should contain_file('/etc/yum.repos.d/zabbix.repo').with_content(/gpgkey=http:\/\/repo.zabbix.com\/RPM-GPG-KEY-ZABBIX-A14FE591/)
+          end
+        end # ends case facts[:operatingsystemmajrelease]
+      when 'Debian'
         it 'should create zabbix.list' do
           should contain_file('/etc/apt/sources.list.d/zabbix.list').with_content(/deb http:\/\/repo.zabbix.com\/zabbix\/3.2\/#{facts[:os]['name'].downcase} #{facts[:os]['lsb']['distcodename']} main/)
         end
@@ -65,7 +55,7 @@ describe 'zabbixagent::preinstall' do
           end
         end # ends case facts[:os]['release']['major']
       end # ends case facts[:osfamily]
-    end # ends context "on #{os}"
+    end # ends context "on #{os} with repo management enabled"
   end # ends on_supported_os.each do |os, facts|
 
   ################################################
